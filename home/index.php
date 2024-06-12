ee57a292d99055484eeeb925942810c4d975811b
Patrick <patrickgmartinez@hotmail.com>
2024-06-12 08:13:12 -0300

    Teste

--- home/index.php
+++ home/index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SAEC - Home</title>
+    <title>SAEC - Home </title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
